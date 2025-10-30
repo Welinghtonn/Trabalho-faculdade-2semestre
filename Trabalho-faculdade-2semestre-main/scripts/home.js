f677c2d8afb7eaabf9771d223f39afcab3cd38a0
Theo Gabriel <theonbgabriel@gmail.com>
2025-10-30 11:37:41 -0300

    commit

--- Trabalho-faculdade-2semestre-main/scripts/home.js
+++ Trabalho-faculdade-2semestre-main/scripts/home.js
@@ -1,42 +1,48 @@
 
 
 async function getDados() {
-    try {
-        const resultado = await fetch("https://bjd9aof9.api.sanity.io/v2025-10-21/data/query/production?query=*%0A++%5B_type+%3D%3D+%27Valores%27%5D%0A%7B%0A++%22Emoji%22+%3A+Emoji.asset+-%3Eurl%2C%0A++++Titulo%2C%0A++++Texto%0A%7D%0A%0A%0A%0A&perspective=drafts ", {
-            method: "GET",
-        })
-        console.log(resultado)
-        const dados = await resultado.json()
-        console.log(dados.result)
+  try {
+    const resultado = await fetch("https://bjd9aof9.api.sanity.io/v2025-10-21/data/query/production?query=*%0A++%5B_type+%3D%3D+%27Valores%27%5D%0A%7B%0A++%22Emoji%22+%3A+Emoji.asset-%3Eurl%2C%0A++++Titulo%2C%0A++++Texto%0A%7D%0A&perspective=drafts");
 
-        const visao = document.getElementById("visao")
-        const missao = document.getElementById("missao")
-        const valores = document.getElementById("valores")
+    const dados = await resultado.json();
+    console.log(dados.result);
 
-        let visaoText = document.createElement("p")
-        let missaoText = document.createElement("p")
-        let valoresText = document.createElement("p")
+    const inicio = document.getElementById("inicio");
 
-        visaoText.innerText = dados.result[0].Texto
-        missaoText.innerText = dados.result[1].Texto
-        valoresText.innerText = dados.result[2].Texto
+    const visao = document.getElementById("visao");
+    const missao = document.getElementById("missao");
+    const valores = document.getElementById("valores");
 
-        visaoText.classList.add("titleCardValores")
-        missaoText.classList.add("titleCardValores")
-        valoresText.classList.add("titleCardValores")
+    // PREENCHE ÍCONES / IMAGENS
+    visao.querySelector("img").src = dados.result[0].Emoji;
+    missao.querySelector("img").src = dados.result[1].Emoji;
+    valores.querySelector("img").src = dados.result[2].Emoji;
 
-        console.log(missaoText)
+    // PREENCHE TEXTOS
+    const visaoText = document.createElement("p");
+    const missaoText = document.createElement("p");
+    const valoresText = document.createElement("p");
 
-        visao.appendChild(visaoText)
-        missao.appendChild(missaoText)
-        valores.appendChild(valoresText)
+    visaoText.textContent = dados.result[0].Texto;
+    missaoText.textContent = dados.result[1].Texto;
+    valoresText.textContent = dados.result[2].Texto;
 
-    } catch (err) {
-        console.error(err)
-    }
+    visaoText.classList.add("titleCardValores");
+    missaoText.classList.add("titleCardValores");
+    valoresText.classList.add("titleCardValores");
+
+    visao.appendChild(visaoText);
+    missao.appendChild(missaoText);
+    valores.appendChild(valoresText);
+
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-getDados()
+renderValores("#app");
+getDados();
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
